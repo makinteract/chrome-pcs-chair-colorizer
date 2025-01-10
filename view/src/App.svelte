542297f2dac86611a185e7d7b40a7b19f89f85a1
Andrea Bianchi <andrea@kaist.ac.kr>
2025-01-11 00:32:33 +0900

    Added all features

--- view/src/App.svelte
+++ view/src/App.svelte
@@ -1,48 +1,76 @@
 <script>
   import './app.css';
-  import { LightSwitch } from '@skeletonlabs/skeleton';
   import '@fortawesome/fontawesome-free/css/all.min.css';
-  import { AppBar } from '@skeletonlabs/skeleton';
   import { setModeCurrent } from '@skeletonlabs/skeleton';
   import ColorPicker from 'svelte-awesome-color-picker';
-  import { changeBg } from './helpers.svelte';
+  import { changeBg, reloadPage, getURL } from './helpers.svelte';
+  import { onMount } from 'svelte';
 
   setModeCurrent(true); // dark mode
 
   let hex = '#e6e909';
+  let correctPage = false;
+
+  onMount(async () => {
+    const url = await getURL();
+    if (url) correctPage = url?.includes('pcschair.org');
+  });
 
   function update() {
-    console.log(hex);
     changeBg(hex);
   }
 </script>
 
-<!-- <AppBar>
-  <svelte:fragment slot="lead"
-    >️
-    <h5 class="h5">PCS-Colorizer</h5>
-  </svelte:fragment>
-  <svelte:fragment slot="trail">
-    <LightSwitch />
-  </svelte:fragment>
-</AppBar> -->
-
-<div class="container">
-  <ColorPicker bind:hex position="responsive" isDialog={false} />
-  <button type="button" class="btn variant-filled mb-3" on:click={update}>
-    <span><i class="fa-solid fa-droplet"></i></span>
-    <span>Update</span>
-  </button>
+<div class="container bg-surface-300">
+  {#if !correctPage}
+    <!-- Show an icon and text in the center of the panel to indicat that this ist he wrong website -->
+    <div class="text-center">
+      <i class="fa-solid fa-exclamation-triangle text-warning"></i>
+      <h3 class="text-warning">This is not PCS Chair</h3>
+    </div>
+  {:else}
+    <ColorPicker bind:hex position="responsive" isDialog={false} />
+    <div class="buttons">
+      <button
+        type="button"
+        class="btn btn-sm variant-filled mb-3"
+        on:click={update}
+      >
+        <span><i class="fa-solid fa-paint-roller"></i></span>
+        <span>Update</span>
+      </button>
+      <button
+        type="button"
+        class="btn btn-sm variant-filled mb-3"
+        on:click={reloadPage}
+      >
+        <span><i class="fa-solid fa-eraser"></i></span>
+        <span>Reset</span>
+      </button>
+    </div>
+  {/if}
 </div>
 
 <style>
   .container {
-    margin-top: 10px;
+    padding-top: 10px;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
     height: 100%;
     width: 100%;
+    min-width: 300px;
+    min-height: 200px;
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: rows;
+  }
+  .btn {
+    margin: 0px 5px 10px 5px;
   }
 </style>
--- view/src/helpers.svelte.ts
+++ view/src/helpers.svelte.ts
@@ -1,12 +1,16 @@
 function colorize(color) {
-  if (document.myInerval) clearInterval(document.myInerval);
+  const prevInterval = localStorage.getItem('interval');
+  if (prevInterval) {
+    clearInterval(prevInterval);
+  }
 
-  document.myInerval = setInterval(() => {
+  const interval = setInterval(() => {
     const papers = document.querySelectorAll('.paper');
     papers.forEach((paper) => {
       paper.style.backgroundColor = color;
     });
   }, 200);
+  localStorage.setItem('interval', interval);
 }
 
 export async function changeBg(color: string) {
@@ -23,18 +27,32 @@ export async function changeBg(color: string) {
   });
 }
 
-// Docs: https://stackoverflow.com/questions/3997659/replace-selected-text-in-contenteditable-div
-
-export async function replaceText(replacementText: string) {
+export async function reloadPage() {
   const [tab] = await chrome.tabs.query({
     active: true,
     currentWindow: true,
   });
-  if (!tab.id) return;
+  if (!tab.id) return 'none'; // no tab selected
 
   await chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: replaceSelectedText,
-    args: [replacementText],
+    func: () => {
+      window.location.reload();
+    },
   });
+}
+
+export async function getURL(): Promise<string | undefined> {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
+  if (!tab.id) return 'none'; // no tab selected
+
+  const [res] = await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => document.URL,
+  });
+
+  return res.result;
 }
